3eb3e6707dbcf36de443ebeb3f04c67a2991d999
Mathieu Benoit <mabenoit@microsoft.com>
2019-07-03 19:42:21 -0400

    Update test-azure-infra.sh

--- test-azure-infra.sh
+++ test-azure-infra.sh
@@ -3,3 +3,6 @@
 az login --service-principal -u $SP_ID -p $SP_SECRET --tenant $SP_TENANT_ID
 az account set -s $SUBSCRIPTION_ID
       
+nodeResourceGroup=$(az aks show -g mabenoitaks -n mabenoitaks --query nodeResourceGroup -o tsv)
+az resource list -g $nodeResourceGroup --resource-type Microsoft.Network/networkInterfaces --query [].name
+az network nic show -g $nodeResourceGroup -n <nic-name> --query "enableAcceleratedNetworking"
