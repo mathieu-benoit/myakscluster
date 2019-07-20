f9da0c6573ed0872f9fe07c698865fc9346880b4
Mathieu Benoit <mabenoit@microsoft.com>
2019-07-20 16:21:31 -0400

    AKS and RG vars

--- create-aks-cluster.sh
+++ create-aks-cluster.sh
@@ -3,31 +3,28 @@
 az login --service-principal -u $SP_ID -p $SP_SECRET --tenant $SP_TENANT_ID
 az account set -s $SUBSCRIPTION_ID
 suffix=$(shuf -i 1000-9999 -n 1)
-
-aksName=$AKS-$suffix
-rgName=$RG-$suffix
       
 # Manage SP and Roles
-#aksClientSecret=$(az ad sp create-for-rbac -n $aksName --skip-assignment --query password -o tsv)
+#aksClientSecret=$(az ad sp create-for-rbac -n $AKS --skip-assignment --query password -o tsv)
 aksClientSecret=$SP_SECRET
-#aksServicePrincipal=$(az ad sp show --id http://$aksName --query appId -o tsv)
+#aksServicePrincipal=$(az ad sp show --id http://$AKS --query appId -o tsv)
 aksServicePrincipal=$SP_ID
       
 # Create Resource Group and Lock
-az group create -n $rgName -l $LOCATION
-az group lock create --lock-type CanNotDelete -n CanNotDelete -g $rgName
+az group create -n $RG -l $LOCATION
+az group lock create --lock-type CanNotDelete -n CanNotDelete -g $RG
       
 # Create VNET
-aksVnetId=$(az network vnet create -g $rgName -n $aksName --address-prefixes 192.168.0.0/16 --subnet-name $aksName --subnet-prefix 192.168.1.0/24 --query id -o tsv)
-subNetId=$(az network vnet subnet show -g $rgName -n $aksName --vnet-name $aksName --query id -o tsv)
+aksVnetId=$(az network vnet create -g $RG -n $AKS --address-prefixes 192.168.0.0/16 --subnet-name $AKS --subnet-prefix 192.168.1.0/24 --query id -o tsv)
+subNetId=$(az network vnet subnet show -g $RG -n $AKS --vnet-name $AKS --query id -o tsv)
 #az role assignment create --assignee $aksServicePrincipal --role "Network Contributor" --scope aksVnetId
       
 # Create the AKS cluster
 k8sVersion=$(az aks get-versions -l $LOCATION --query 'orchestrators[-1].orchestratorVersion' -o tsv)
 az aks create \
             -l $LOCATION \
-            -n $aksName \
-            -g $rgName \
+            -n $AKS \
+            -g $RG \
             -k $k8sVersion \
             -s $NODE_SIZE \
             -c $NODE_COUNT \
@@ -37,15 +34,15 @@ az aks create \
             --vnet-subnet-id $subNetId
       
 # Disable K8S dashboard
-az aks disable-addons -a kube-dashboard -n $aksName -g $rgName
+az aks disable-addons -a kube-dashboard -n $AKS -g $RG
       
 # Azure Monitor for containers
-workspaceResourceId=$(az resource create -g $rgName --resource-type "Microsoft.OperationalInsights/workspaces" -n $rgName -l $LOCATION -p '{"sku":{"Name":"Standalone"}}' --query id -o tsv)
+workspaceResourceId=$(az resource create -g $RG --resource-type "Microsoft.OperationalInsights/workspaces" -n $AKS -l $LOCATION -p '{"sku":{"Name":"Standalone"}}' --query id -o tsv)
 # FIXME az role assignment create --assignee $aksServicePrincipal --role Contributor --scope workspaceResourceId
-az aks enable-addons -a monitoring -n $aksName -g $rgName --workspace-resource-id $workspaceResourceId
+az aks enable-addons -a monitoring -n $AKS -g $RG --workspace-resource-id $workspaceResourceId
       
 # Get kubeconfig to be able to run following kubectl commands
-az aks get-credentials -n $aksName -g $rgName
+az aks get-credentials -n $AKS -g $RG
       
 # Kured
 kuredVersion=1.2.0
