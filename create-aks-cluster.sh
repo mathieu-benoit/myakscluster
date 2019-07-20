a672903a1bcedb94887a6778e735aa2e0772638f
Mathieu Benoit <mabenoit@microsoft.com>
2019-07-20 14:03:54 -0700

    Accelerated Networking
    * Standard_DS2_v2
    
    * Multi-stage to add Tests
    
    * Update azure-pipeline.yml
    
    * Update azure-pipeline.yml
    
    * Update azure-pipeline.yml
    
    * Update azure-pipeline.yml
    
    * Create test-azure-infra.sh
    
    * Update azure-pipeline.yml
    
    * rgName and aksName variables
    
    * Update test-azure-infra.sh
    
    * Update azure-pipeline.yml
    
    * for + if for nics
    
    * condition to create aks cluster
    
    * Update azure-pipeline.yml
    
    * Update azure-pipeline.yml
    
    * Update test-azure-infra.sh
    
    * Update test-azure-infra.sh
    
    * pass variables across stages
    
    * AKS and RG vars
    
    * Update create-aks-cluster.sh
    
    * Update azure-pipeline.yml
    
    * Update azure-pipeline.yml
    
    * Update azure-pipeline.yml

--- create-aks-cluster.sh
+++ create-aks-cluster.sh
@@ -2,29 +2,28 @@
 
 az login --service-principal -u $SP_ID -p $SP_SECRET --tenant $SP_TENANT_ID
 az account set -s $SUBSCRIPTION_ID
-suffix=$(shuf -i 1000-9999 -n 1)
       
 # Manage SP and Roles
-#aksClientSecret=$(az ad sp create-for-rbac -n $AKS-$suffix --skip-assignment --query password -o tsv)
+#aksClientSecret=$(az ad sp create-for-rbac -n $AKS --skip-assignment --query password -o tsv)
 aksClientSecret=$SP_SECRET
-#aksServicePrincipal=$(az ad sp show --id http://$AKS-$suffix --query appId -o tsv)
+#aksServicePrincipal=$(az ad sp show --id http://$AKS --query appId -o tsv)
 aksServicePrincipal=$SP_ID
       
 # Create Resource Group and Lock
-az group create -n $RG-$suffix -l $LOCATION
-az group lock create --lock-type CanNotDelete -n CanNotDelete -g $RG-$suffix
+az group create -n $RG -l $LOCATION
+az group lock create --lock-type CanNotDelete -n CanNotDelete -g $RG
       
 # Create VNET
-aksVnetId=$(az network vnet create -g $RG-$suffix -n $AKS-$suffix --address-prefixes 192.168.0.0/16 --subnet-name $AKS-$suffix --subnet-prefix 192.168.1.0/24 --query id -o tsv)
-subNetId=$(az network vnet subnet show -g $RG-$suffix -n $AKS-$suffix --vnet-name $AKS-$suffix --query id -o tsv)
+aksVnetId=$(az network vnet create -g $RG -n $AKS --address-prefixes 192.168.0.0/16 --subnet-name $AKS --subnet-prefix 192.168.1.0/24 --query id -o tsv)
+subNetId=$(az network vnet subnet show -g $RG -n $AKS --vnet-name $AKS --query id -o tsv)
 #az role assignment create --assignee $aksServicePrincipal --role "Network Contributor" --scope aksVnetId
       
 # Create the AKS cluster
 k8sVersion=$(az aks get-versions -l $LOCATION --query 'orchestrators[-1].orchestratorVersion' -o tsv)
 az aks create \
             -l $LOCATION \
-            -n $AKS-$suffix \
-            -g $RG-$suffix \
+            -n $AKS \
+            -g $RG \
             -k $k8sVersion \
             -s $NODE_SIZE \
             -c $NODE_COUNT \
@@ -34,15 +33,15 @@ az aks create \
             --vnet-subnet-id $subNetId
       
 # Disable K8S dashboard
-az aks disable-addons -a kube-dashboard -n $AKS-$suffix -g $RG-$suffix
+az aks disable-addons -a kube-dashboard -n $AKS -g $RG
       
 # Azure Monitor for containers
-workspaceResourceId=$(az resource create -g $RG-$suffix --resource-type "Microsoft.OperationalInsights/workspaces" -n $RG-$suffix -l $LOCATION -p '{"sku":{"Name":"Standalone"}}' --query id -o tsv)
+workspaceResourceId=$(az resource create -g $RG --resource-type "Microsoft.OperationalInsights/workspaces" -n $AKS -l $LOCATION -p '{"sku":{"Name":"Standalone"}}' --query id -o tsv)
 # FIXME az role assignment create --assignee $aksServicePrincipal --role Contributor --scope workspaceResourceId
-az aks enable-addons -a monitoring -n $AKS-$suffix -g $RG-$suffix --workspace-resource-id $workspaceResourceId
+az aks enable-addons -a monitoring -n $AKS -g $RG --workspace-resource-id $workspaceResourceId
       
 # Get kubeconfig to be able to run following kubectl commands
-az aks get-credentials -n $AKS-$suffix -g $RG-$suffix
+az aks get-credentials -n $AKS -g $RG
       
 # Kured
 kuredVersion=1.2.0
---
+++ test-azure-infra.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+az login --service-principal -u $SP_ID -p $SP_SECRET --tenant $SP_TENANT_ID
+az account set -s $SUBSCRIPTION_ID
+      
+# Accelerated Networking check
+nodeResourceGroup=$(az aks show -g $RG -n $AKS --query nodeResourceGroup -o tsv)
+for nic in $(az resource list -g $nodeResourceGroup --resource-type Microsoft.Network/networkInterfaces --query [].name -o tsv)
+do 
+      enabled=$(az network nic show -g $nodeResourceGroup -n $nic --query "enableAcceleratedNetworking")
+      if [ $enabled = "false" ]; then
+            1>&2 echo "AcceleratedNetworking not enabled"
+      fi 
+done
