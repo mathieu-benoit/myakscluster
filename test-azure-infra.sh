988212b1201740f2577a777626db84a2398d3da5
Mathieu Benoit <mabenoit@microsoft.com>
2020-03-17 16:27:10 -0400

    fix acceleratednetworking check

--- test-azure-infra.sh
+++ test-azure-infra.sh
@@ -6,7 +6,7 @@ az account set -s $SUBSCRIPTION_ID
 # Accelerated Networking check
 nodeResourceGroup=$(az aks show -g $RG -n $AKS --query nodeResourceGroup -o tsv)
 nodesVmss=$(az vmss list -g $nodeResourceGroup -o tsv --query [0].name)
-for enabled in $(az vmss nic list -g $RG --vmss-name $nodesVmss --query [].enableAcceleratedNetworking -o tsv)
+for enabled in $(az vmss nic list -g $nodeResourceGroup --vmss-name $nodesVmss --query [].enableAcceleratedNetworking -o tsv)
 do 
       if [ $enabled = "false" ]; then
             1>&2 echo "AcceleratedNetworking not enabled"
