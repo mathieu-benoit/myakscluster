abcd8e988b3d406e6cc34abebba7f3746d6d98e3
Mathieu Benoit <mabenoit@microsoft.com>
2020-03-20 21:39:46 -0400

    Delete post-create-aks-cluster.sh

--- post-create-aks-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ssh -i $JUMPBOX_SSH_KEY vsts@$JUMPBOX_IP_ADDRESS
-
-ls -la
-
-az login --service-principal -u $SP_ID -p $SP_SECRET --tenant $SP_TENANT_ID
-az account set -s $SUBSCRIPTION_ID
