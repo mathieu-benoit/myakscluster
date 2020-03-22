54dd5e40f38deeca9deb7938b47a398878b12f45
Mathieu Benoit <mabenoit@microsoft.com>
2020-03-21 22:25:52 -0400

    Update and rename cloud-init.txt to cloud-init.sh

---
+++ cloud-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt update -y 
+sudo apt upgrade -y
+
+# jq
+sudo apt install jq -y
+
+# unzip
+sudo apt install unzip -y
+
+# azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# helm
+curl -sL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | sudo bash
+
+# kubectl
+echo "Installing kubectl..."
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+echo "alias k=kubectl" | sudo tee ~/.bash_aliases
