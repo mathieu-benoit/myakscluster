1d2468657dd4f45d8448c987328de9a7e18d7606
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-04 23:37:25 -0400

    Create variables.tf

---
+++ tf/variables.tf
@@ -0,0 +1,7 @@
+variable "aks_name" {
+  description = "Name of the AKS service name and all other Azure services associated to it: resource group, vnet, etc."
+}
+
+variable "location" {
+  description = "Location of the resources."
+}
