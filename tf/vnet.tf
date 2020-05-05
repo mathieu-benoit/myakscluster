90ffc6bd5e3c1c3afaef3845689558ce6d05234d
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-05 00:42:10 -0400

    Create vnet.tf

---
+++ tf/vnet.tf
@@ -0,0 +1,23 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  address_space       = [var.virtual_network_address_prefix]
+
+  subnet {
+    name           = var.aks_subnet_name
+    address_prefix = var.aks_subnet_address_prefix
+  }
+
+  subnet {
+    name           = "appgwsubnet"
+    address_prefix = var.app_gateway_subnet_address_prefix
+  }
+}
+
+resource "azurerm_subnet" "aks_nodes_subnet" {
+  name                 = "aks_nodes_subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
