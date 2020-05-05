804671bc2c49e3aee00baaeb39ed4892fd631906
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-04 23:52:07 -0400

    Update aks.tf

--- tf/aks.tf
+++ tf/aks.tf
@@ -1,16 +1,35 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.aks_name
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    name            = "system"
+    node_count      = 3
+    vm_size         = "Standard_DS2_v2"
+    type            = "VirtualMachineScaleSets"
+    os_disk_size_gb = var.os_disk_size_gb
+    vnet_subnet_id  = var.azure_subnet_id
   }
 
   identity {
     type = "SystemAssigned"
+  }
+  
+  network_profile {
+    network_plugin     = "azure"
+    network_policy     = "calico"
+    load_balancer_sku  = "standard"
+    service_cidr       = var.service_cidr
+    dns_service_ip     = var.dns_service_ip
+    docker_bridge_cidr = var.docker_bridge_cidr
+  }
+  
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+    }
   }
 }
