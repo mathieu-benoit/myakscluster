4428acc34a42f19b84673230beb96251abb61c00
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-05 00:10:31 -0400

    Update aks.tf

--- tf/aks.tf
+++ tf/aks.tf
@@ -1,8 +1,10 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.aks_name
+  name                    = var.aks_name
+  location                = var.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  dns_prefix              = var.aks_name
+  kubernetes_version      = var.k8s_version
+  #private_cluster_enabled = true
 
   default_node_pool {
     name            = "system"
@@ -11,6 +13,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type            = "VirtualMachineScaleSets"
     os_disk_size_gb = var.os_disk_size_gb
     vnet_subnet_id  = var.azure_subnet_id
+    #node_labels
+    #availability_zones
   }
 
   identity {
@@ -31,5 +35,20 @@ resource "azurerm_kubernetes_cluster" "aks" {
       enabled                    = true
       log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
     }
+    kube_dashboard {
+      enabled = false
+    }
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "usernodepool" {
+  name                  = "linuxuser"
+  os_type               = "linux"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 3
+  #node_labels
+  #os_disk_size_gb
+  #availability_zones
+  #vnet_subnet_id
 }
