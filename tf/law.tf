ca98c7d24411a4fc6a53b05bcbd4740b184b6db4
Mathieu Benoit <mabenoit@microsoft.com>
2020-05-05 00:29:20 -0400

    Create law.tf

---
+++ tf/law.tf
@@ -0,0 +1,19 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "las" {
+  solution_name         = "Containers"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.law.id
+  workspace_name        = azurerm_log_analytics_workspace.law.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/Containers"
+  }
+}
