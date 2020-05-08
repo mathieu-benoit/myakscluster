3d0a1258d1444670dac89207c77c8c53c65cf6ae
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2020-05-08 09:17:10 -0400

    azurerm tf 2.9.0

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,11 @@
 # https://github.com/terraform-providers/terraform-provider-azurerm
 provider "azurerm" {
-  version = "=2.8.0"
+  version = "=2.9.0"
   features {}
 }
+
+# + Terraform State in Blog storage account access (vnet peering + vnet link in private dns)
+
+# + Azure DevOps interaction to update variable groups (ACR sp credentials and AKS sp credentials)?
+# https://github.com/microsoft/terraform-provider-azuredevops/blob/master/examples/azdo-based-cicd/main.tf#L56
+# Or alternatively changing this for Azure Key Vault? Maybe better?
