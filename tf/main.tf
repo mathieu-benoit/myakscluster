c276dd89671993a8f0af1b75d7727d6fae029fa2
mathieu-benoit <mabenoit@microsoft.com>
2020-06-07 00:05:15 -0400

    azurerm 2.13.0 + azuread 0.10.0

--- tf/main.tf
+++ tf/main.tf
@@ -1,11 +1,11 @@
 # https://github.com/terraform-providers/terraform-provider-azurerm
 provider "azurerm" {
-  version = "=2.12.0"
+  version = "=2.13.0"
   features {}
 }
 
 provider "azuread" {
-  version = "=0.8.0"
+  version = "=0.10.0"
 }
 
 provider "external" {
